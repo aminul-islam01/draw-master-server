288bf72525e2608a303325049d586325f5524a0f
aminul-islam01 <abiddut953@gmail.com>
2023-06-09 18:13:15 +0600

    add jwt

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const app = express();
@@ -10,13 +11,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const password = process.env.DB_PASS;
-const name = process.env.DB_USER;
-
-console.log(password, name)
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aws78to.mongodb.net/?retryWrites=true&w=majority`;
-// const uri = "mongodb+srv://<username>:<password>@cluster0.aws78to.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -33,6 +29,12 @@ async function run() {
         await client.connect();
 
         const usersCollection = client.db("draw-masterDB").collection("users");
+
+        app.post('/jsonwebtoken', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+            res.send({ token })
+        })
 
         app.get('/users', async(req, res) => {
             const result = await usersCollection.find().toArray();
